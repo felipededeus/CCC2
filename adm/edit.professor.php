5041faff6192a0fc285f92b65ea6e2026b028db3
felipededeus <felipeamadoedeus@yahoo.com.br>
2018-10-18 10:19:23 -0300

    CRUD Professor

---
+++ adm/edit.professor.php
@@ -0,0 +1,92 @@
+<?php include 'header.template.php';  ?> <!-- Importando Header -->
+
+<?php
+
+$id = $_POST['id'];
+
+$nome = $_POST['nome'];
+
+$snome = $_POST['snome'];
+
+$email = $_POST['email'];
+
+$username = $_POST['username'];
+
+$usernameold = $_POST['usernameold'];
+
+$dtnasc = $_POST['dtnasc'];
+
+
+include '../conexao.class.php';
+
+if (!isset($id)) {
+  echo '
+           <div class="alert alert-danger" role="alert">
+           Erro: Parâmetros Inválidos!
+           </div>
+           ';
+           exit();
+}
+
+
+
+if ($username != $usernameold) {
+   //Verificando se Já existe um user com o mesmo nome cadastrado no sistema
+        
+        $sqli = "SELECT username FROM admin WHERE username= :username UNION SELECT username FROM professor WHERE username= :username UNION SELECT username FROM pedagogo WHERE username= :username";
+        $stmi = Conexao::prepare($sqli);
+        $stmi->bindParam(':username', $username);
+        $stmi->execute();
+
+
+ if ($stmi->rowCount()> 0) {     
+
+    $_SESSION ['jaexiste'] = "Erro[046]: Esse nome de usuário já existe!";  
+    header('Location: adm.professor.php');
+    exit();
+
+  }
+}
+
+
+
+
+$sql = "UPDATE professor SET nome= :nome, snome= :snome, email= :email, username= :username, dtnasc= :dtnasc  WHERE id= :id";
+// Atualize na Tabela Pedagogo definindo nome antigo por nome novo sobrenome antigo por sobrenome novo, e assim sucessivamente,onde id seja igual a id solicitada pelo usuário
+
+          $stm = Conexao::prepare($sql);
+          $stm->bindParam(':id', $id);
+          $stm->bindParam(':nome', $nome);
+          $stm->bindParam(':snome', $snome);
+           $stm->bindParam(':email', $email);
+          $stm->bindParam(':username', $username);
+          $stm->bindParam(':dtnasc', $dtnasc);
+
+
+
+          if($stm->execute()){
+            
+           $_SESSION ['editresultid'] =  "1" ; // Gera Session se deu certo
+           header('Location: adm.professor.php'); // Manda pra página de onde o user veio
+           exit(); // Para o Script     
+
+
+
+         } else{
+
+          $_SESSION ['editresultid'] =  "0" ; // Gera Session se deu certo
+          header('Location: adm.professor.php'); // Manda pra página de onde o user veio
+          exit(); // Para o Script       
+         } 
+
+
+
+         // Os Sessions são usados para gerar a notificação na página de origem
+      
+
+?>
+
+<?php include '../footer.php';  ?> <!-- Importando Rodapé -->
+</body>
+</html> 
+
